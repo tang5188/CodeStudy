cf18e343ce5144d79bd45b5cd15f5a0cda47c4f2
tang5188 <tang.peng.ju@163.com>
2022-10-07 22:58:08 +0800

    feat: 数据验证

---
+++ BlazorApp/BlazorApp/Data/Child.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class Child
+    {
+        [Required]
+        public string Code { get; set; }
+
+        [StringLength(5, ErrorMessage = "长度超出")]
+        public string Name { get; set; }
+    }
+}
